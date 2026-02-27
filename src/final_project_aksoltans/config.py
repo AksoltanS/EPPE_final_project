13c497fc6662d2dff125aee926ecadb944372c39
AksoltanS <134161060+AksoltanS@users.noreply.github.com>
2026-02-27 15:39:32 +0100

    Updated project config and added dependencies

--- src/final_project_aksoltans/config.py
+++ src/final_project_aksoltans/config.py
@@ -7,7 +7,18 @@ ROOT: Path = SRC.joinpath("..", "..").resolve()
 
 BLD: Path = ROOT.joinpath("bld").resolve()
 
-
 DOCUMENTS: Path = ROOT.joinpath("documents").resolve()
 
 TEMPLATE_GROUPS: tuple[str, ...] = ("marital_status", "highest_qualification")
+
+DATA = SRC / "data"
+DATA_RAW = DATA / "raw"
+
+FOLLOW_BACKS_RAW = DATA_RAW / "follow_backs.csv"
+
+BLD_DATA = BLD / "data"
+BLD_FIGURES = BLD / "figures"
+
+FOLLOW_BACKS_ANALYSIS = BLD_DATA / "follow_backs_analysis.parquet"
+FIG1_DATA = BLD_FIGURES / "fig1_data.csv"
+FIG1_PNG = BLD_FIGURES / "fig1_followbacks_type.png"
