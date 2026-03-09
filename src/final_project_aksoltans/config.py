08ff819f8cf255b5e5b24cc0a2f98d223ce20c13
AksoltanS <134161060+AksoltanS@users.noreply.github.com>
2026-03-09 23:25:57 +0100

    Refactor and clean config file

--- src/final_project_aksoltans/config.py
+++ src/final_project_aksoltans/config.py
@@ -6,46 +6,71 @@ SRC: Path = Path(__file__).parent.resolve()
 ROOT: Path = SRC.joinpath("..", "..").resolve()
 BLD: Path = ROOT.joinpath("bld").resolve()
 
-TEMPLATE_GROUPS: tuple[str, ...] = ("marital_status", "highest_qualification")
-
-DATA = SRC / "data"
-DATA_RAW = DATA / "raw"
-BLD_DATA = BLD / "data"
-BLD_FIGURES = BLD / "figures"
+DATA_RAW = SRC / "data" / "raw"
 
 FOLLOW_BACKS_RAW = DATA_RAW / "follow_backs.csv"
 SUBJECT_POOL_RAW = DATA_RAW / "subject_pool.csv"
 SUBJECT_POOL_SCRAMBLED_RAW = DATA_RAW / "subject_pool_scrambled.csv"
 
+
+BLD_DATA = BLD / "data"
+BLD_FIGURES = BLD / "figures"
+BLD_TABLES = BLD / "tables"
+
 FOLLOW_BACKS_ANALYSIS = BLD_DATA / "follow_backs_analysis.parquet"
 SUBJECT_POOL_ANALYSIS = BLD_DATA / "subject_pool_analysis.parquet"
 
-BOT_VARS: list[str] = ["bot_gender", "bot_race", "bot_uni"]
-CONTROLS_CATEGORICAL: list[str] = ["continent", "profession", "female"]
-CONTROLS_CONTINUOUS: list[str] = [
+
+FIG1_DATA = BLD_FIGURES / "fig1_data.parquet"
+FIG1_PNG = BLD_FIGURES / "fig1_followbacks_type.png"
+
+FIG2_DATA = BLD_FIGURES / "fig2_data.parquet"
+FIG2_PNG = BLD_FIGURES / "fig2_marginals.png"
+
+FIG3A_DATA = BLD_FIGURES / "fig3a_data.parquet"
+FIG3A_PNG = BLD_FIGURES / "fig3a_interaction.png"
+
+FIG3B_DATA = BLD_FIGURES / "fig3b_data.parquet"
+FIG3B_PNG = BLD_FIGURES / "fig3b_interactions.png"
+
+TABLE1A_TEX = BLD_TABLES / "Table1a_Descriptives_Qualitative.tex"
+TABLE1B_TEX = BLD_TABLES / "Table1b_Descriptives_Quantitative.tex"
+
+BOT_VARS = ["bot_gender", "bot_race", "bot_uni"]
+
+CONTROLS_CATEGORICAL = ["continent", "profession", "female"]
+
+CONTROLS_CONTINUOUS = [
     "above_median_year_created",
     "follow_diversity",
     "background_pic",
     "above_median_followers_count",
     "above_median_friends_count",
 ]
-FE_GROUPS: list[str] = ["wave", "strata", "missfit"]
-PVAL_DISPLAY_THRESHOLD: float = 0.001
 
+FE_VARS = ["wave", "strata", "missfit"]
 
-FIG1_DATA = BLD_FIGURES / "fig1_data.csv"
-FIG1_PNG = BLD_FIGURES / "fig1_followbacks_type.png"
-FIG2_GENDER_DATA = BLD_FIGURES / "fig2_gender.csv"
-FIG2_RACE_DATA = BLD_FIGURES / "fig2_race.csv"
-FIG2_UNI_DATA = BLD_FIGURES / "fig2_uni.csv"
-FIG2_PNG = BLD_FIGURES / "fig2_marginals.png"
-FIG2_CONTROLS_DATA = BLD_FIGURES / "fig2_controls.csv"
+PVAL_DISPLAY_THRESHOLD = 0.001
 
-FIG3A_DATA = BLD_FIGURES / "fig3a_data.csv"
-FIG3A_PNG = BLD_FIGURES / "fig3a_interaction.png"
+TABLE1_QUAL_VARS = [
+    "gender",
+    "continent",
+    "profession",
+    "race",
+    "follow_diversity",
+    "background_pic",
+]
 
-FIG3B_DATA = BLD_FIGURES / "fig3b_data.csv"
-FIG3B_PNG = BLD_FIGURES / "fig3b_interactions.png"
+TABLE1_QUAL_VARS_SCRAMBLED = [
+    "TopUni_10",
+    "verified",
+]
 
-TABLE1A_TEX: Path = BLD_FIGURES / "table1a.tex"
-TABLE1B_TEX: Path = BLD_FIGURES / "table1b.tex"
+TABLE1_QUANT_VARS = [
+    "followers_count",
+    "friends_count",
+    "statuses_count",
+    "favourites_count",
+    "listed_count",
+    "share_mobile",
+]
